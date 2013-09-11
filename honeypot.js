08579062307ef81ebec1d8f6b5cec47abf1aef63
Daniel Cousens <RommelVR@users.noreply.github.com>
2013-09-11 14:27:11 +1000

    Simplifies control flow

--- honeypot.js
+++ honeypot.js
@@ -1,21 +1,23 @@
 var dns = require('./dns');
+var fs = require('fs');
 var http = require('http');
 
 // Globals
+var index = fs.readFileSync('index.html');
+var image = fs.readFileSync('bear.png');
 var ip = '10.0.0.1';
-var index, image;
 
-// Set-up the DNS server
-var dns_server = dns.createServer(function (req, res) {
+// The DNS server
+var dnsServer = dns.createServer(function (req, res) {
 	if (req.opcode == 0) {
 		console.log('Request from', req.remoteAddress + ':' + req.remotePort, 'for', req.qname);
 
 		res.end(ip);
 	}
-});
+}).listen(53, ip);
 
-// Set-up the HTTP server
-var http_server = http.createServer(function (req, res) {
+// The HTTP server
+var httpServer = http.createServer(function (req, res) {
 	if (req.url.indexOf('png') != -1) {
 		res.writeHead(200, {'Content-Type': 'image/png'});
 		res.end(image);
@@ -23,17 +25,4 @@ var http_server = http.createServer(function (req, res) {
 		res.writeHead(200, {'Content-Type': 'text/html'});
 		res.end(index);
 	}
-});
-
-// Load all necessary resources, then bind the servers
-var fs = require('fs');
-fs.readFile('index.html', function(error, data) {
-	index = data;
-
-	fs.readFile('bear.png', function(error, data) {
-		image = data;
-
-		dns_server.listen(53, ip);
-		http_server.listen(80, ip);
-	});
-});
+}).listen(80, ip);
